faa43b8415254483055f16f0b6fc7b23b59b0244
Steve-black <porndittha77@gmail.com>
2022-03-03 13:25:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "iOS Developer"],
+        strings: ["Web Developer", "iOS Developer", "Mobile Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "iOS Developer"],
+        strings: ["Web Developer", "iOS Developer", "Mobile Developer"],
         typeSpeed: 200,
         backSpeed: 120,
         loop: true
